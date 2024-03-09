932d4d7d6daf1595933c22f9efb45df4e6ea4e27
Max Kaiser <max@family.com>
2024-03-09 22:27:02 +0100

    added enemy_spawner script

--- Scripts/enemy_spawner.gd
+++ Scripts/enemy_spawner.gd
@@ -8,6 +8,9 @@ func _on_timer_timeout():
 	spawn_enemy()
 
 func spawn_enemy():
+	var spawn_positions_array = spawn_positions.get_children()
+	var random_spawn_position = spawn_positions_array.pick_random()
+	
 	var enemy_instance = enemy_scene.instantiate()
-	enemy_instance.global_position = Vector2(1350, 300)
+	enemy_instance.global_position = random_spawn_position.global_position
 	add_child(enemy_instance)
