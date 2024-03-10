2ce19a2acea58581755c90db42af7bd11166135a
Max Kaiser <max@family.com>
2024-03-10 11:55:34 +0100

    added game over sceen and panel

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -5,21 +5,30 @@ var score = 0
 
 @onready var player = $Player
 @onready var hud = $UI/HUD
+@onready var ui = $UI
+
+var game_over_scene = preload("res://Scenes/game_over_screen.tscn")
 
 # sets score to 0 at start of game
 func _ready() -> void:
 	hud.set_score_label(score)
+	hud.set_lives(player_lives)
 
 func _on_death_zone_area_entered(area: Area2D) -> void:
-	area.kill_enemy()
+	area.queue_free()
 
 func _on_player_took_damage() -> void:
 	player_lives -= 1
+	hud.set_lives(player_lives)
 	if player_lives == 0:
 		print("Game Over")
 		player.die()
-	else:
-		print(player_lives)
+		
+		await get_tree().create_timer(0.5).timeout
+		
+		var gos = game_over_scene.instantiate()
+		gos.set_score(score)
+		ui.add_child(gos)
 
 func _on_enemy_spawner_enemy_spawned(enemy_instance: Variant) -> void:
 	enemy_instance.connect("died", _on_enemy_died)
